0d30d824f49dae163019f937f4fe1d3131e24719
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-01-21 21:33:05 -0300

    lista 3

--- mod02_listas_tuplas_dicionarios/ex01.py
+++ mod02_listas_tuplas_dicionarios/ex01.py
@@ -1,1 +1,13 @@
-# TODO: implementar
+telefonou = input('Resposta: ')
+local = input('Resposta: ')
+mora = input('Resposta: ')
+devia = input('Resposta: ')
+trabalhou = input('Resposta: ')
+
+perguntas: {
+    "Telefonou para a vítima?": telefonou,
+    "Esteve no local do crime?": local,
+    "Mora perto da vítima?": mora,
+    "Devia para a vítima?": devia,
+    "Já trabalhou com a vítima?": trabalhou
+}
