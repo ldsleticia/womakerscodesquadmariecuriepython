8818d4ca70b53f16c0dd0dd66e2d246a95145bac
Bea-Martins <bmc_c@outlook.com>
2024-01-22 22:11:50 -0300

    feat: resolucao ex10 modulo 00

--- mod00_conceitos_basicos/ex10.py
+++ mod00_conceitos_basicos/ex10.py
@@ -1,1 +1,10 @@
-# TODO: implementar
+''' Faça um Programa que utilize 4 variáveis como preferir e no final print uma mensagem amigável utilizando as variáveis criadas.
+Exemplos de variáveis: nome, idade, lugar, profissão ....
+Exemplo de retorno: Olá Maria, prazer te conhecer. Sou de São Paulo também e estou migrando de área. '''
+
+nome = input('Qual é o seu nome? ')
+cidade = input('Qual cidade você mora? ')
+animal = input('Qual é o seu animal preferido? ')
+livro = input('Qual é o nome do seu livro favorito? ')
+
+print('Oii {}, Prazer em conhece-la! Ouvi dizer que {} é um lugar muito agradável. Acho {} um animal íncrivel hehe. Não conhecia o livro {}, obrigada pela dica <3'.format(nome,cidade,animal,livro))
