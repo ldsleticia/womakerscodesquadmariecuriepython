069532a3db0dfa260c190ef747bc9b69ea54f736
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-01-23 20:29:02 -0300

    Update ex01.py
    Solução exercício 1 mod 3

--- mod02_listas_tuplas_dicionarios/ex01.py
+++ mod02_listas_tuplas_dicionarios/ex01.py
@@ -1,1 +1,30 @@
-# TODO: implementar
+#Exercício 1 Módulo 3
+def classificar_suspeito():
+
+    perguntas = [
+        "Telefonou para a vítima?",
+        "Esteve no local do crime?",
+        "Mora perto da vítima?",
+        "Devia para a vítima?",
+        "Já trabalhou com a vítima?"
+    ]
+
+    respostas_positivas = 0
+
+    for pergunta in perguntas:
+        resposta = input(pergunta + " (sim/não) ").lower()
+        if resposta == "sim":
+            respostas_positivas += 1
+
+    if respostas_positivas == 2:
+        classificacao = "Suspeita"
+    elif 3 <= respostas_positivas <= 4:
+        classificacao = "Cúmplice"
+    elif respostas_positivas == 5:
+        classificacao = "Assassino"
+    else:
+        classificacao = "Inocente"
+
+    print(f"A classificação da pessoa no crime é: {classificacao}")
+
+classificar_suspeito()
