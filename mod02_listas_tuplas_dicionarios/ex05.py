cb333747711c98f7ed59de81e209fc7cf1f272fd
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-01-23 20:30:21 -0300

    Create ex05.py
    Solução exercício 05 mod 3

---
+++ mod02_listas_tuplas_dicionarios/ex05.py
@@ -0,0 +1,9 @@
+#Exercício 5 Módulo 3
+
+tupla1 = ("Ada Lovelace", "Grace Hopper", "Hedy Lamarr")
+tupla2 = ("Katherine Johnson", "Annie Easley", "Dorothy Vaughan")
+
+# Concatenando as tuplas
+tupla_concatenada = tupla1 + tupla2
+
+print("Mulheres históricas na tecnologia:", tupla_concatenada)
