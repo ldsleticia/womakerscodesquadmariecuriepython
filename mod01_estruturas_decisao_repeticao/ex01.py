06e7311a5c72007d220f0e26626994ed2ff7ab44
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-01-23 20:27:28 -0300

    Update ex01.py

--- mod01_estruturas_decisao_repeticao/ex01.py
+++ mod01_estruturas_decisao_repeticao/ex01.py
@@ -1,1 +1,18 @@
-# TODO: implementar
+#Exercício 1 Módulo 2
+
+def imprimir_maior_numero():
+    
+    while True:
+    
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+    
+        if num1 > num2:
+            print(f"O maior número é: {int(num1)}")
+        elif num2 > num1:
+            print(f"O maior número é: {int(num2)}")
+        else:
+            print("Os números são iguais.")
+
+
+imprimir_maior_numero()
